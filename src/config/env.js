6d986051a334a9a2b559b782e60f3fa1f0dc1017
dilshodturab <avlomberdioglid@gmail.com>
2026-09-14 17:45:25 +0500

    chore: correcting the key names and installing the package for development

--- src/config/env.js
+++ src/config/env.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 module.exports.PORT = process.env.PORT;
-module.exports.ENV = process.env.NODE_ENV;
+module.exports.NODE_ENV = process.env.NODE_ENV;
 module.exports.DB_URL = process.env.DB_URL || "postgres://do:1234@localhost:5432/pgdbfortest";
 module.exports.REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 module.exports.SECRET = process.env.SECRET;
