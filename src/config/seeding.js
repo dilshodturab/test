fb8fd54495768403250ec055b9bf0890f79945c3
dilshodturab <avlomberdioglid@gmail.com>
2026-09-14 22:35:24 +0500

    chore: making seeding log looking good.

--- src/config/seeding.js
+++ src/config/seeding.js
@@ -13,15 +13,15 @@ module.exports.seedTables = async () => {
     await pool.query(`
       create table if not exists users (
         id uuid primary key default gen_random_uuid(),
-        created_at timestamp default current_timestamp
+        created_at timestamp default current_timestamp,
         username varchar(50) not null unique,
         email varchar(100) not null unique,
-        password varchar(255) not null,
+        password varchar(255) not null
         );
 
       create table if not exists products (
         id uuid primary key default gen_random_uuid(),
-        created_at timestamp default current_timestamp
+        created_at timestamp default current_timestamp,
         name varchar(100) not null unique,
         price numeric(10, 2) not null,
         stock_quantity int not null default 0
@@ -29,13 +29,13 @@ module.exports.seedTables = async () => {
 
       create table if not exists orders (
         id uuid primary key default gen_random_uuid(),
-        created_at timestamp default current_timestamp
+        created_at timestamp default current_timestamp,
         status order_status not null default 'pending',
         idem_key varchar(255),
         products uuid[] default '{}'
       );
       `);
-    console.log("Yo twin all of your tables are seeded.");
+    console.log("✅ Yo twin all of your tables are seeded.");
   } catch (error) {
     console.log("Error table seeding: ", error);
   }
