c9887366be8b5d8a8b1b37d7fc451708a1f5852c
dilshodturab <avlomberdioglid@gmail.com>
2026-09-17 12:17:38 +0500

    chore: cache invalidation added to the products.

--- src/services/products.service.js
+++ src/services/products.service.js
@@ -1,9 +1,12 @@
 const { CustomThrowError } = require("../config/custom-errors");
 const { redisClient } = require("../config/redis-connect");
 const productsRepository = require("../repositories/products.repository")
+const CACHE_KEY = "products:all";
+const CACHE_TTL = 60 * 60 * 24;
 
 module.exports.create = async (data) => {
   try {
+
     await productsRepository.create(data);
   } catch (err) {
     if (err.code === "23505") {
@@ -11,11 +14,13 @@ module.exports.create = async (data) => {
     }
     throw err;
   }
+
+  try {
+		await redisClient.del(CACHE_KEY);
+	} catch (error) { console.log("Redis del error:", error.message)}
 }
 
 module.exports.all = async () => {
-  const CACHE_KEY = "products:all";
-  const CACHE_TTL = 60 * 60 * 24;
 
   try {
     const cached = await redisClient.get(CACHE_KEY);
